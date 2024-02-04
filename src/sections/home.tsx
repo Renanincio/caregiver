91f1dcfd9c0efb676a9ff82cbbbe0ca4c9d7cb7f
Renanincio <renanincioprgmdr@gmail.com>
2024-02-04 10:21:23 -0300

    swiper js in home and services modificated

--- src/sections/home.tsx
+++ src/sections/home.tsx
@@ -1,14 +1,28 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/autoplay";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export const Home = () => {
   return (
-    <section id="home" className="bg-services min-h-screen">
+    <section id="home" className="bg-services min-h-screen flex">
       <div className="max-w-[1280px] mx-auto flex h-screen justify-evenly items-center">
+        <div className="flex flex-col w-[700px]">
+          <h1 className="font-semibold capitalize text-[48px] leading-10 mb-8 mt-16">
+            Filipe Alexandre
+            <br /> psicólogo formado pela Unisuam
+          </h1>
+          <p className="w-[400px] italic capitalize leading-relaxed">
+            fundador da página no Instagram papo_psi1. Aonde é divulgados um
+            pouco da psicologia de maneira mais fácil.
+          </p>
+          <button className=" py-[10px] w-[230px] mt-16 font-bold italic capitalize border-[1px] bg-greenSecondary border-greenSecondary hover:bg-transparent transition-all delay-[.3] text-header hover:text-greenPrimary rounded-btn">
+            Saiba mais
+          </button>
+        </div>
+
         <Swiper
           spaceBetween={30}
           centeredSlides={true}
@@ -17,51 +31,37 @@ export const Home = () => {
             delay: 5000,
             disableOnInteraction: false,
           }}
-          pagination={{
-            clickable: false,
-          }}
+          pagination={false}
           navigation={false}
           modules={[Autoplay, Pagination, Navigation]}
-          className="mySwiper max-w-[1280px] mx-auto flex h-screen justify-evenly items-center"
+          className="mySwiper h-full w-[475px]"
         >
-          <SwiperSlide className="flex flex-row justify-evenly items-center">
-            <div className="flex flex-col">
-              <h1 className="font-semibold capitalize text-[48px] leading-10 mb-8 mt-16">
-                Filipe Alexandre
-                <br /> psicólogo formado pela Unisuam
-              </h1>
-              <p className="w-[400px] italic capitalize leading-relaxed">
-                fundador da página no Instagram papo_psi1. Aonde é divulgados um
-                pouco da psicologia de maneira mais fácil.
-              </p>
-              <button className=" py-[10px] w-[230px] mt-16 font-bold italic capitalize border-[1px] bg-greenSecondary border-greenSecondary hover:bg-transparent transition-all delay-[.3] text-header hover:text-greenPrimary rounded-btn">
-                Saiba mais
-              </button>
-            </div>
+          <SwiperSlide className="h-full">
             <img
               src="./Filipe1.jpg"
               alt="foto de um psicólogo"
               className="w-[475px] h-full rounded-btn"
             />
           </SwiperSlide>
-          <SwiperSlide className="flex flex-row justify-evenly items-center">
-            <div className="flex flex-col">
-              <h1 className="font-semibold capitalize text-[48px] leading-10 mb-8 mt-16">
-                Maria de Fátima Barbosa
-                <br /> neuropsicóloga
-              </h1>
-              <p className="w-[400px] italic capitalize leading-relaxed">
-                também sargento das forças aéreas da aeronáutica, formada pela
-                universidade Estácio de Sá.
-              </p>
-              <button className=" py-[10px] w-[230px] mt-16 font-bold italic capitalize border-[1px] bg-greenSecondary border-greenSecondary hover:bg-transparent transition-all delay-[.3] text-header hover:text-greenPrimary rounded-btn">
-                Saiba mais
-              </button>
-            </div>
+          <SwiperSlide className="h-full">
             <img
-              src="./filipe2.jpg"
+              src="./Filipe2.jpg"
               alt="foto de um psicólogo"
               className="w-[475px] h-full rounded-btn"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="h-full">
+            <img
+              src="./Filipe3.jpg"
+              alt="foto de um psicólogo"
+              className="w-[475px] h-full rounded-btn"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="h-full">
+            <img
+              src="./Filipe4.jpg"
+              alt="foto de um psicólogo"
+              className="w-[550px] h-full rounded-btn"
             />
           </SwiperSlide>
         </Swiper>
--- src/sections/services.tsx
+++ src/sections/services.tsx
@@ -1,3 +1,11 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
 export const Services = () => {
   return (
     <section
@@ -9,64 +17,85 @@ export const Services = () => {
         <div className="h-[1px] w-[300px] bg-greenPrimary mb-8 mt-2 mx-auto" />
 
         <ul className="flex  justify-between items-start">
-          <li>
-            <h2 className="text-[42px] uppercase font-medium mb-8 mt-16">
-              Terapia Individual
-            </h2>
-            <img
-              src="/terapiaindi.jpeg"
-              className="w-[475px] h-[325px] m-auto rounded-e-btn"
-            />
+          <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            pagination={false}
+            modules={[EffectCoverflow, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <li className="flex flex-col justify-center text-center items-center">
+                <h2 className="text-[42px] uppercase font-medium mb-8 mt-16">
+                  Terapia Individual
+                </h2>
+                <img
+                  src="/terapiaindi.jpeg"
+                  className="w-[475px] h-[325px] m-auto rounded-e-btn"
+                />
 
-            <p className="text-[16px] font-normal w-[550px] mt-8">
-              A terapia individual é um contexto terapêutico singular,
-              proporcionando um espaço dedicado à exploração profunda de
-              pensamentos e sentimentos, focado na abordagem de questões
-              específicas que possam influenciar a vida do indivíduo. Nossos
-              profissionais, embasados nas melhores evidências em psicologia,
-              comprometem-se a estabelecer um ambiente terapêutico seguro e
-              confidencial, promovendo a expressão aberta e a análise cuidadosa
-              de experiências íntimas. Ao longo desse processo, nosso psicólogo
-              especializado emprega abordagens terapêuticas embasadas em
-              evidências, visando compreender de maneira aprofundada os desafios
-              emocionais e oferecer intervenções práticas. A terapia individual,
-              moldada pelas melhores práticas da psicologia, representa uma
-              jornada adaptativa de autoexploração, apoio emocional e
-              desenvolvimento pessoal. Estamos dedicados a guiar o indivíduo
-              rumo ao seu bem-estar emocional e à consecução de metas, ancorados
-              nas evidências científicas mais robustas.
-            </p>
-          </li>
+                <p className="text-[16px] font-normal w-[550px] mt-8">
+                  A terapia individual é um contexto terapêutico singular,
+                  proporcionando um espaço dedicado à exploração profunda de
+                  pensamentos e sentimentos, focado na abordagem de questões
+                  específicas que possam influenciar a vida do indivíduo. Nossos
+                  profissionais, embasados nas melhores evidências em
+                  psicologia, comprometem-se a estabelecer um ambiente
+                  terapêutico seguro e confidencial, promovendo a expressão
+                  aberta e a análise cuidadosa de experiências íntimas. Ao longo
+                  desse processo, nosso psicólogo especializado emprega
+                  abordagens terapêuticas embasadas em evidências, visando
+                  compreender de maneira aprofundada os desafios emocionais e
+                  oferecer intervenções práticas. A terapia individual, moldada
+                  pelas melhores práticas da psicologia, representa uma jornada
+                  adaptativa de autoexploração, apoio emocional e
+                  desenvolvimento pessoal. Estamos dedicados a guiar o indivíduo
+                  rumo ao seu bem-estar emocional e à consecução de metas,
+                  ancorados nas evidências científicas mais robustas.
+                </p>
+              </li>
+            </SwiperSlide>
 
-          <li>
-            <h2 className="text-[42px] uppercase font-medium mb-8 mt-16">
-              Diagnóstico
-            </h2>
-            <img
-              src="/diagnóstico.jpeg"
-              className="w-[475px] h-[325px] m-auto rounded-e-btn"
-            />
+            <SwiperSlide >
+              <li className="flex flex-col justify-center text-center items-center">
+                <h2 className="text-[42px] uppercase font-medium mb-8 mt-16">
+                  Diagnóstico
+                </h2>
+                <img
+                  src="/diagnóstico.jpeg"
+                  className="w-[475px] h-[325px] m-auto rounded-e-btn"
+                />
 
-            <p className="text-[16px] font-normal w-[550px] mt-8">
-              Em nossa abordagem no Insight, oferecemos testes
-              diagnósticos realizados por profissionais especializados em
-              psicologia. Estes testes são aplicados para detectar e avaliar
-              possíveis transtornos mentais, proporcionando uma compreensão mais
-              profunda de suas emoções e desafios pessoais, como ansiedade,
-              depressão, autismo, transtornos de personalidade, entre outros. Ao
-              escolher nossos serviços de testes diagnósticos, você está dando o
-              primeiro passo em direção a uma jornada de autoconhecimento e
-              bem-estar emocional. Estamos aqui para oferecer suporte e
-              orientação personalizada.
-            </p>
-          </li>
+                <p className="text-[16px] font-normal w-[550px] mt-8">
+                  Em nossa abordagem no Insight, oferecemos testes diagnósticos
+                  realizados por profissionais especializados em psicologia.
+                  Estes testes são aplicados para detectar e avaliar possíveis
+                  transtornos mentais, proporcionando uma compreensão mais
+                  profunda de suas emoções e desafios pessoais, como ansiedade,
+                  depressão, autismo, transtornos de personalidade, entre
+                  outros. Ao escolher nossos serviços de testes diagnósticos,
+                  você está dando o primeiro passo em direção a uma jornada de
+                  autoconhecimento e bem-estar emocional. Estamos aqui para
+                  oferecer suporte e orientação personalizada.
+                </p>
+              </li>
+            </SwiperSlide>
+          </Swiper>
         </ul>
 
         <button className="italic w-[225px] py-[6px] text-[20px] mx-auto rounded-btn my-14 border-[1px] border-greenSecondary font-semibold hover:bg-greenSecondary transition-all delay-[.3] hover:text-about">
           Entre em contato
         </button>
       </div>
-      
     </section>
   );
 };
