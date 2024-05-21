96e76787272e7a0961f088b24f9a0cacfdb8ba4e
Renanincio <renanincioprgmdr@gmail.com>
2024-05-21 20:06:48 -0300

    gbdfg

--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -1,6 +1,7 @@
 import * as RadioGroup from "@radix-ui/react-radio-group";
 
 export const Contact = () => {
+  
   return (
     <>
       <section id="contact" className="min-h-screen bg-contact flex items-center">
@@ -11,26 +12,32 @@ export const Contact = () => {
           <p className="text-white text-[18px] my-8">
             Pegue um café e entre em contato conosco!
           </p>
-          <form className="flex flex-col gap-4 text-start max-[900px]:px-8">
+          <form action="https://formsubmit.co/clinicapsicologicainsigth@gmail.com" method="POST" className="flex flex-col gap-4 text-start max-[900px]:px-8">
             <input
               type="text"
               placeholder="Nome"
+              name="nome"
               className="p-2 capitalize border-solid border-[1px] border-greenPrimary bg-white outline-none"
             />
             <input
               type="email"
               placeholder="email"
+             name="email"
               className="p-2 capitalize border-solid border-[1px] border-greenPrimary bg-white outline-none"
             />
+            <input type="hidden" name="_next" value="https://caregiver-nu.vercel.app/"></input>
+            <input type="hidden" name="_captcha" value="false"></input>
             <textarea
               className="p-2 capitalize border-solid border-[1px] border-greenPrimary bg-white outline-none"
               name="Mensagem"
               rows={1}
               placeholder="Como podemos ajudar?"
+       
             />
             <RadioGroup.Root
               className="RadioGroupRoot flex flex-col gap-4 text-start"
               defaultValue="default"
+              name="Tipo de atendimento:"
               aria-label="View density"
             >
               <legend>Tipo de atendimento:</legend>
@@ -39,6 +46,7 @@ export const Contact = () => {
                   className="RadioGroupItem bg-white w-[15px] h-[15px] rounded-[100%] hover:bg-greenPrimary"
                   value="Presencial"
                   id="r2"
+             
                 >
                   <RadioGroup.Indicator className="RadioGroupIndicator flex items-center justify-center w-full h-full relative after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-greenPrimary" />
                 </RadioGroup.Item>
@@ -50,6 +58,7 @@ export const Contact = () => {
                   className="RadioGroupItem bg-white w-[15px] h-[15px] rounded-[100%] hover:bg-greenPrimary"
                   value="Online"
                   id="r3"
+             
                 >
                   <RadioGroup.Indicator className="RadioGroupIndicator flex items-center justify-center w-full h-full relative after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-greenPrimary" />
                 </RadioGroup.Item>
